0c57239b0171e57e6c59ceb8eee404afab10892a
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-06-03 17:17:56 +0900

    removed bootstrap

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import { Row, Col } from "reactstrap";
+import { Grid } from "@material-ui/core";
 
 class CurrentWeather extends Component {
   render() {
     return (
-      <Row>
-        <Col sm="12" md={{ size: 8, offset: 2 }}>
-          <div>
-            <div className='big-data'>
-              <i className={ `wi wi-yahoo-${ this.props.iconId } main-icon` }></i>
-              <h2>{this.props.temp}&deg;</h2>
-            </div>
-            <h2 className='main-description'>{ this.props.description }</h2>
-            <h1 className='main-city'>{ this.props.city }</h1>
+      <Grid item xl={6} alignContent="center" justify="center">
+          <div className='big-data'>
+            <i className={ `wi wi-yahoo-${ this.props.iconId } main-icon` }></i>
+            <h2>{this.props.temp}&deg;</h2>
           </div>
-        </Col>
-      </Row>
+          <h2 className='main-description'>{ this.props.description }</h2>
+          <h1 className='main-city'>{ this.props.city }</h1>
+      </Grid>
     )
   }
 }
--- src/components/User.js
+++ src/components/User.js
@@ -13,17 +13,15 @@ class User extends Component {
     return (
       <div>
         { this.props.user === null ? (
-          <MenuItem onClick={this.props.signIn}>
-            <Button color="primary" variant="outlined">Sign In</Button>
-          </MenuItem>
-        ) : (
-          <MenuItem onClick={this.props.signOut}>
-            <Button color="secondary" variant="outlined">Sign Out</Button>
-          </MenuItem>
-        )
+            <MenuItem onClick={this.props.signIn}>
+              <Button color="primary" variant="outlined">Sign In</Button>
+            </MenuItem>
+          ) : (
+            <MenuItem onClick={this.props.signOut}>
+              <Button color="secondary" variant="outlined">Sign Out</Button>
+            </MenuItem>
+          )
         }
-        
-        
       </div>
     )
   }
