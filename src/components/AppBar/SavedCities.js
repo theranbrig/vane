7b57a61d660870f5bf5c27c8b161de43730ef42a
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-10-03 17:17:49 +0900

    added user saved cities and fixed orange color

--- src/components/AppBar/SavedCities.js
+++ src/components/AppBar/SavedCities.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
-import { MenuItem } from "@material-ui/core";
+import { MenuItem } from '@material-ui/core';
 
 class SavedCities extends Component {
-  render() {
-    return (
-      <div>
-        <MenuItem>
-          Phuket
-        </MenuItem>
-        <MenuItem>
-          Rome
-        </MenuItem>
-        <MenuItem>
-          Chicago
-        </MenuItem>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				{this.props.savedCities == null ? (
+					<MenuItem>Login to Save Cities</MenuItem>
+				) : (
+					this.props.savedCities.map((city, index) => (
+						<MenuItem key={index} onClick={() => this.props.apiSearch(city.city)}>
+							{city.city}
+						</MenuItem>
+					))
+				)}
+			</div>
+		);
+	}
 }
 
 export default SavedCities;
